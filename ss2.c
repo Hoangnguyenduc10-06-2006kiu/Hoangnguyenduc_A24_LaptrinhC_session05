29c2f6aeb92483921d940d00b59d9b08f6362546
Hoangnguyenduc10-06-2006kiu <gochoctap102@gmail.com>
2024-11-26 08:22:53 +0700

    Add files via upload

---
+++ ss2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	int numBerOne=20;
+	int numBer ;
+	printf("moi ban nhap so");
+	scanf("%d",&numBer); 
+	while(numBer!=numBerOne){
+		printf("ban doan sai roi, moi ban nhap so");
+		scanf("%d",&numBer); 
+		
+	}
+	printf("moi ban nhap so");
+return 0;	
+	                     
+	 
+}
